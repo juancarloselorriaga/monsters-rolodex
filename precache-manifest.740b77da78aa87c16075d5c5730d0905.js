c85559795c19e57529216c86b3b9d9d17ab46306
juancarloselorriaga <juancarloselorriaga@gmail.com>
2019-08-22 23:47:03 -0500

    Updates

---
+++ precache-manifest.740b77da78aa87c16075d5c5730d0905.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a42bdb1dcc84ed28ce964a129345507e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5e122c61f467736e18b3",
+    "url": "/monsters-rolodex/static/css/main.d986f3b4.chunk.css"
+  },
+  {
+    "revision": "dfc56958e7d46aab54f3",
+    "url": "/monsters-rolodex/static/js/2.c2d9bc4f.chunk.js"
+  },
+  {
+    "revision": "5e122c61f467736e18b3",
+    "url": "/monsters-rolodex/static/js/main.baf3233d.chunk.js"
+  },
+  {
+    "revision": "9ea9c29140c1f80c8044",
+    "url": "/monsters-rolodex/static/js/runtime~main.3810dc4c.js"
+  }
+]);
